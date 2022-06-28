f7356573fce8b847e351e399ce82e79277c444bb
TheYX144 <94316832+TheYX144@users.noreply.github.com>
2022-06-28 15:10:27 -0400

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,10 @@
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile0`, function (sprite, location) {
     game.over(true)
 })
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    Render.toggleViewMode()
+})
 let mySprite = Render.getRenderSpriteVariable()
 tiles.setCurrentTilemap(tilemap`level1`)
 tiles.placeOnRandomTile(mySprite, assets.tile`myTile`)
+info.startCountup()
